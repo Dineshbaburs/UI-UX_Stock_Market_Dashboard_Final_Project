482c425675583603c8e27211191b8ea590e8df93
Dinesh <rsdineshbaburajkumar@gmail.com>
2025-09-25 08:59:04 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// Application Configuration
+window.TechStock = {
+  API_KEY: "44HUOFRTTY0CLKRP",
+  CACHE_DURATION: 5 * 60 * 1000,
+  UPDATE_INTERVAL: 3 * 60 * 1000,
+  API_DELAY: 12000,
+  MAX_HISTORY: 30
+};
